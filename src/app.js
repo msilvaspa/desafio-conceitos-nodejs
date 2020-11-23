b70dcff48a19a7443d690106d42f640bf4d2e466
Mateus Alves <mateus.alves@abcbrasil.com.br>
2020-11-23 12:50:34 -0300

    done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,54 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).send(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const repo = {...request.body, likes: 0, id: uuid()}
+  repositories.push(repo)
+  return response.status(201).send(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const repoIndex = repositories.findIndex((r) => r.id === id);
+  if (repoIndex === -1) return response.status(400).send();
+
+  const repoUpdated = {
+    id,
+    url,
+    title,
+    techs,
+    likes: repositories[repoIndex].likes,
+  };
+  repositories[repoIndex] = repoUpdated;
+  return response.json(repoUpdated);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const repoIndex = repositories.findIndex((r) => r.id === id);
+  if (repoIndex >= 0) {
+    repositories.splice(repoIndex, 1)
+  } else {
+    return response.status(400).send()
+  }
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex((r) => r.id === id);
+  if (repoIndex === -1) {
+    return response.status(400).send();
+  }
+  repositories[repoIndex].likes += 1;
+  
+  return response.json(repositories[repoIndex]);
 });
 
 module.exports = app;
